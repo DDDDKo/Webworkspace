bd3109ace9c5f60c04643c5d1ccaed5aadb13819
shimtaewook <to1583@naver.com>
2024-03-20 11:53:21 +0900

    2024-03-20 11:50

--- js/10.JS_Array/array2.js
+++ js/10.JS_Array/array2.js
@@ -1,14 +1,41 @@
 /*
     배열 고차 함수
-    - 
+    - 배열의 불변성을 최대한 보장하여 부수효과를 제거한 메서드 (부수효과 = 원본에 미치는 영향)
 */
 const array = [7, 11, 20, 1, 8, 3, 16, 2, 19, 9, 10, 4, 13, 12, 14, 17, 15, 5, 18, 6];
 
 /*
-    sort(비교 함수) : 
-    - 
+    sort(비교 함수) : 배열에 요소를 적절하게 정렬
+    - 단순하게 .sort() 형태로 정렬하면 사전식 정렬을 하게 됨, 
 */
+// array.sort();
+// console.log(array);
 
+const asc = function (x, y){
+    return x - y;
+}
+// const desc = function (x, y){
+//     return y - x;
+// }
+array.sort(asc);
+console.log(array);
+// array.sort(desc);
+// console.log(array);
+array.sort(function (x, y) { return y - x});
+console.log(array);
+
+const objects = [
+    { x : 9, y: -5},
+    { x : -2, y: 0},
+    { x : 0, y: -3},
+    { x : 6, y: 5}
+];
+console.log(objects);
+
+objects.sort(function (a, b){ 
+    return a.y - b.y;
+});
+console.log(objects);
 
 console.log('==============================');
 
